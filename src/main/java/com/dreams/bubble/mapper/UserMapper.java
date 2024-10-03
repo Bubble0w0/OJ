265f8c101bc4a9ae1c419570876c656062090b2f
Bubble0w0 <1372660072@qq.com>
2024-10-03 18:09:32 +0800

    add:基于模板创建并初始化

---
+++ src/main/java/com/dreams/bubble/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.dreams.bubble.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dreams.bubble.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
